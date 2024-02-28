a9a4528b2432d5cc8ff73929e1d7b616070932df
DanteTerry <sinasa88@gmail.com>
2024-02-28 23:43:59 +0100

    initial  commit

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import { Jost, Playfair_Display } from "next/font/google";
+import "./globals.css";
+
+const play_fair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--playfair-display",
+});
+
+const jost = Jost({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--jost",
+});
+
+export const metadata = {
+  title: "BitWay Rentals - Streamlining Your Property Rental Experience",
+  description:
+    "Discover seamless property rentals with BitWay! Explore our user-friendly platform for hassle-free leasing experiences. From apartments to vacation homes, find your perfect rental match effortlessly. Join BitWay today for a smarter way to rent!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${play_fair.variable} ${jost.variable}`}>
+      <body className="font-jost text-primary">{children}</body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        playfair: ["var(--playfair-display)"],
+        jost: ["var(--jost)"],
+      },
+
+      colors: {
+        "primary-500": rgba(62, 84, 235, 1),
+      },
+
+      textColor: {
+        primary: "rgba(15, 16, 21, 1)",
       },
     },
   },
