92d4c63619acb76d8f32aba67528718680839b22
DanteTerry <sinasa88@gmail.com>
2024-03-01 20:35:59 +0100

    fixed star icon import failed

--- components/ReviewCard.jsx
+++ components/ReviewCard.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import star from "@/assets/ReviewStar.svg";
+import star from "@/assets/reviewStar.svg";
 import reviewer from "@/assets/reviewer.svg";
 
 function ReviewCard() {
