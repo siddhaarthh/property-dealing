c1c927a3f4751ef1bf7596cb55242d4322a1bec0
DanteTerry <sinasa88@gmail.com>
2024-04-24 23:47:00 +0200

    fixed handle submit function

--- components/Schedule.jsx
+++ components/Schedule.jsx
@@ -7,7 +7,11 @@ function Schedule() {
   const {
     register,
     formState: { isSubmitting, errors },
+    handleSubmit,
   } = useForm();
+  const onsubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="h-max rounded-lg px-5  lg:w-2/6">
       <h6 className=" mb-5 text-xl font-bold uppercase">Schedule a tour</h6>
@@ -28,75 +32,76 @@ function Schedule() {
       </div>
 
       <div className="flex flex-col gap-5">
-        <div className="w-full">
-          <label htmlFor="name" className="mb-2 font-bold">
-            Name
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="h-[50px] w-full rounded-xl border border-[#A4A6AC33] p-3 text-primary-500 outline-none"
-            placeholder="Enter Your Name"
-            {...register("name", { required: true })}
-          />
-          {errors.name && (
-            <p className="text-red-500">This field is required</p>
-          )}
-        </div>
-        <div className="w-full">
-          <label htmlFor="email" className="mb-2 font-bold">
-            Email
-          </label>
-          <input
-            id="email"
-            type="text"
-            className="h-[50px] w-full rounded-xl border border-[#A4A6AC33] p-3 text-primary-500 outline-none"
-            placeholder="Enter Your Email"
-            {...register("email", { required: true })}
-          />
+        <form onSubmit={handleSubmit(onsubmit)}>
+          <div className="w-full">
+            <label htmlFor="name" className="mb-2 font-bold">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              className="h-[50px] w-full rounded-xl border border-[#A4A6AC33] p-3 text-primary-500 outline-none"
+              placeholder="Enter Your Name"
+              {...register("name", { required: true })}
+            />
+            {errors.name && (
+              <p className="text-red-500">This field is required</p>
+            )}
+          </div>
+          <div className="w-full">
+            <label htmlFor="email" className="mb-2 font-bold">
+              Email
+            </label>
+            <input
+              id="email"
+              type="text"
+              className="h-[50px] w-full rounded-xl border border-[#A4A6AC33] p-3 text-primary-500 outline-none"
+              placeholder="Enter Your Email"
+              {...register("email", { required: true })}
+            />
 
-          {errors.email && (
-            <p className="text-red-500">This field is required</p>
-          )}
-        </div>
-        <div className="w-full">
-          <label htmlFor="phoneNumber" className="mb-2 font-bold">
-            Phone Number
-          </label>
-          <input
-            id="phoneNumber"
-            type="number"
-            className="h-[50px] w-full rounded-xl border border-[#A4A6AC33] p-3 text-primary-500 outline-none"
-            placeholder="Enter Your Phone Number"
-            {...register("phoneNumber", { required: true })}
-          />
-          {errors.phoneNumber && (
-            <p className="text-red-500">This field is required</p>
-          )}
-        </div>
-        <div className="w-full">
-          <label htmlFor="message" className="mb-2 font-bold">
-            Message
-          </label>
-          <textarea
-            id="message"
-            type="text"
-            className="h-[100px] w-full rounded-xl border border-[#A4A6AC33] p-3 text-primary-500 outline-none"
-            placeholder="Enter Your Message"
-            {...register("message", { required: true })}
-          />
-          {errors.message && (
-            <p className="text-red-500">This field is required</p>
-          )}
-        </div>
-
-        <button
-          disabled={isSubmitting}
-          className="rounded-xl bg-primary-500 p-3 text-white"
-          onClick={() => console.log("Request sent")}
-        >
-          Send a request
-        </button>
+            {errors.email && (
+              <p className="text-red-500">This field is required</p>
+            )}
+          </div>
+          <div className="w-full">
+            <label htmlFor="phoneNumber" className="mb-2 font-bold">
+              Phone Number
+            </label>
+            <input
+              id="phoneNumber"
+              type="number"
+              className="h-[50px] w-full rounded-xl border border-[#A4A6AC33] p-3 text-primary-500 outline-none"
+              placeholder="Enter Your Phone Number"
+              {...register("phoneNumber", { required: true })}
+            />
+            {errors.phoneNumber && (
+              <p className="text-red-500">This field is required</p>
+            )}
+          </div>
+          <div className="w-full">
+            <label htmlFor="message" className="mb-2 font-bold">
+              Message
+            </label>
+            <textarea
+              id="message"
+              type="text"
+              className="h-[100px] w-full rounded-xl border border-[#A4A6AC33] p-3 text-primary-500 outline-none"
+              placeholder="Enter Your Message"
+              {...register("message", { required: true })}
+            />
+            {errors.message && (
+              <p className="text-red-500">This field is required</p>
+            )}
+          </div>
+          <button
+            disabled={isSubmitting}
+            className="rounded-xl bg-primary-500 p-3 text-white"
+            onClick={() => console.log("Request sent")}
+          >
+            Send a request
+          </button>
+        </form>
       </div>
     </div>
   );
